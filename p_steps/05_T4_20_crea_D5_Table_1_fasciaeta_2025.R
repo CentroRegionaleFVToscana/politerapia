a331c1bde10d48c77f523983590a059e3b3d527d
sabrinagiometto <sabrina.giometto@gmail.com>
2026-09-14 11:34:35 +0200

    script adapted to the study protocol

--- p_steps/05_T4_20_crea_D5_Table_1_fasciaeta_2025.R
+++ p_steps/05_T4_20_crea_D5_Table_1_fasciaeta_2025.R
@@ -18,79 +18,81 @@ if (TEST){
 
 
 # load data
-
-
 data <- readRDS(file = paste0(thisdirinput, "/D3_coorte_con_caratterizzazione.rds"))
 data <- as.data.table(data)
 
-#   
-# 
-# for (j in fasce_eta) {
-#   
-#   # Create D5 with sociodemographic characteristics
-#   D5_nocov <- data[drug==j, .(
-#                 N          = .N,
-#                 age_median = median(age),
-#                 age_q1 = quantile(age, probs = 0.25),
-#                 age_q3 = quantile(age, probs = 0.75),
-#                 genere_F_N = sum(genere=="F"),
-#                 genere_F_p = round(sum(genere=="F")/.N,3)*100),
-#                 .(period_first, ASL, user_type)]
-#   
-#   # create D5 with binary covariates
+# to be removed
+fasce_eta <- c("40-64", "65-84", "85+")
+
+
+for (j in fasce_eta) {
+
+  # Create D5 with sociodemographic characteristics
+  D5_nocov <- data[fasciaeta==j, .(
+                N          = .N,
+                genere_F_N = sum(genere=="F"),
+                genere_F_p = round(sum(genere=="F")/.N,3)*100),
+                # aggiungere ulteriori covariate
+                .(ASL)]
+  
+  
+  
+  
+
+  # create D5 with binary covariates
 #   covariates_binary <- c("iperten", "cardioisc", "infart", "arit", "ictus", "tia",
-#                          "scompcard", "dislip", "diab", "renal", "cortic", 
+#                          "scompcard", "dislip", "diab", "renal", "cortic",
 #                          "antiang", "antitromb", "ipolip", "antidiab", "bifosf",
-#                          "switch_apalu", "switch_enzalu", "switch_darolu", 
+#                          "switch_apalu", "switch_enzalu", "switch_darolu",
 #                          "switch_other_oncol")
-#   
+# 
 #   D5_cov <- NULL
-#   
+# 
 #   for (i in covariates_binary) {
-#     
+# 
 #     tmp <- data[drug==j, .(
 #                 N = .N,
 #                 tmp_N = sum(get(i)==1),
 #                 tmp_p = round(sum(get(i)==1)/.N,3)*100),
 #                 .(period_first, ASL, user_type)]
-#     
+# 
 #     setnames(tmp,"tmp_N",paste0(i, "_N"))
 #     setnames(tmp,"tmp_p",paste0(i, "_p"))
-#     
+# 
 #     if (is.null(D5_cov)) {
-#       
+# 
 #       D5_cov <- tmp
-#       
+# 
 #     } else {
-#       
+# 
 #       D5_cov <- merge(D5_cov, tmp, by = c("period_first", "ASL", "user_type", "N"))
 #     }
-#     
+# 
 #   }
-#   
+# 
 #   # create the final D5 by merging the previous two
 #   D5 <- merge(D5_nocov, D5_cov, by = c("period_first", "ASL", "user_type","N"), all = F)
-#   
+# 
 #   assign(paste0("D5_",j), D5)
 # 
 # }
 # 
 # for (j in drug_names) {
-#   
+# 
 #   saveRDS(get(paste0("D5_", j)), file = paste0(thisdiroutput, "/D5_", j, ".rds"))
 #   write.csv(get(paste0("D5_", j)), file = paste0(thisdiroutput, "/D5_", j, ".csv"))
-# 
-#   # # save
-#   # if (TEST & type_data_test=="simulation") {
-#   #   
-#   #   saveRDS(D5, file = paste0(thisdiroutput, "/D5_from_simulation_", j, ".rds"))
-#   #   write.csv(D5, file = paste0(thisdiroutput, "/D5_from_simulation_", j, ".csv"))
-#   #   
-#   # } else if (TEST & type_data_test=="dummy") {
-#   #   
-#   #   saveRDS(D5, file = paste0(thisdiroutput, "/D5_from_dummy_data_", j,".rds"))
-#   #   write.csv(D5, file = paste0(thisdiroutput, "/D5_from_dummy_data_", j,".csv"))
-#   #   
-#   # }
-#   
-# }
+
+  # # save
+  # if (TEST & type_data_test=="simulation") {
+  #
+  #   saveRDS(D5, file = paste0(thisdiroutput, "/D5_from_simulation_", j, ".rds"))
+  #   write.csv(D5, file = paste0(thisdiroutput, "/D5_from_simulation_", j, ".csv"))
+  #
+  # } else if (TEST & type_data_test=="dummy") {
+  #
+  #   saveRDS(D5, file = paste0(thisdiroutput, "/D5_from_dummy_data_", j,".rds"))
+  #   write.csv(D5, file = paste0(thisdiroutput, "/D5_from_dummy_data_", j,".csv"))
+  #
+  # }
+
+}
