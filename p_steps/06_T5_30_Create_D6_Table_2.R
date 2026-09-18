e7ebfc5708314542b0d520ba6e2dfb0df7f101c7
Elena264-F <elenaferrati@gmail.com>
2026-09-18 16:31:30 +0200

    D6 Table 2 creation

---
+++ p_steps/06_T5_30_Create_D6_Table_2.R
@@ -0,0 +1,254 @@
+
+#### D6_Table_2 ----
+
+
+# authors: Sabrina Giometto, Elena Ferrati
+
+# v 0.1 17 Sep 2026 Creation of D6 started
+
+
+
+print('CREATE D6_Table_2')
+
+
+# assign directories
+
+if (TEST){ 
+  testname <- "test_D6_Tabella_2_asl_2025"
+  thisdirinput <- paste0(file.path(dirtest, testname), "/")
+  thisdiroutput <- file.path(dirtest,testname,"g_output")
+  dir.create(thisdiroutput, showWarnings = F)
+}else{
+  thisdirinput <- direxp
+  thisdiroutput <- direxp
+}
+
+
+# load
+
+for (j in asl) {
+  
+  D5 <- read.csv(paste0(thisdirinput, "D5_Table_2_combinazioni_farmaci_2025_", j, ".csv"))
+  
+  D5 <- as.data.table(D5)
+  
+  assign(paste0("D5_Table_2_combinazioni_farmaci_2025_",j), D5)
+  
+}
+
+
+
+
+# helpers
+
+add_empty_row <- function(j){
+  
+  j <- j + 1
+  tab_nice[, cell := ""]
+  setnames(tab_nice, "cell", paste0("cell_",j))
+  return(j)
+}
+
+descriptive_N_perc <- function(j, covar) {
+  
+  j <- j + 1
+  varN <- paste0(covar,"N")
+  varP <- paste0(covar,"p")
+  tab_nice[, cell := paste0(
+    formatC(get(varN), format = "f", digits = 0, big.mark = ".",
+            decimal.mark = ","), " (",
+    formatC(get(varP), format = "f", digits = 1, big.mark = ".",
+            decimal.mark = ","),"%)"
+  )]
+  setnames(tab_nice, "cell", paste0("cell_",j))
+  return(j)
+}
+
+descriptive_median_q1q3 <- function(j, covar) {
+  
+  j <- j + 1
+  varM <- paste0(covar,"_median")
+  varQ1 <- paste0(covar,"_q1")
+  varQ3 <- paste0(covar,"_q3")
+  tab_nice[, cell := paste0(get(varM), " (", get(varQ1), " - ",
+                            get(varQ3), ")")]
+  setnames(tab_nice, "cell", paste0("cell_", j))
+  return(j)
+}
+
+
+
+#########################################
+# POPULATE ROWS
+
+for (k in asl) {
+  
+  tab_nice <- copy(get(paste0("D5_Table_2_combinazioni_farmaci_2025_", k)))
+  
+  # row 0
+  row_header_1 <- c()
+  j            <- -1
+  
+  # row 1
+  row_header_1 <- c(row_header_1, "Totale")
+  j <- j + 1
+  tab_nice[, cell := as.character(N)]
+  setnames(tab_nice, "cell", paste0("cell_", j))
+  
+  # row 2
+  row_header_1 <- c(row_header_1, "Combinazioni ATC IV livello (*)")
+  j <- add_empty_row(j)
+  
+  # row 3
+  #row_header_1 <- c(row_header_1, "N pazienti")
+  # j <- add_empty_row(j)
+  # 
+  # # row 4
+  # row_header_1 <- c(row_header_1, "% sul totale in politerapia")
+  # j <- add_empty_row(j)
+  
+  # row 5
+  row_header_1 <- c(row_header_1, variable_names_full[[k]][1])
+  j <- descriptive_N_perc(j, paste0(variable_names_full[[k]][1], "_"))
+  
+  # row 6
+  row_header_1 <- c(row_header_1, variable_names_full[[k]][2])
+  j <- descriptive_N_perc(j, paste0(variable_names_full[[k]][2], "_"))
+  
+  # row 7
+  row_header_1 <- c(row_header_1, variable_names_full[[k]][3])
+  j <- descriptive_N_perc(j, paste0(variable_names_full[[k]][3], "_"))
+  
+  # row 8
+  row_header_1 <- c(row_header_1, variable_names_full[[k]][4])
+  j <- descriptive_N_perc(j, paste0(variable_names_full[[k]][4], "_"))
+  
+  # row 9
+  row_header_1 <- c(row_header_1, variable_names_full[[k]][5])
+  j <- descriptive_N_perc(j, paste0(variable_names_full[[k]][5], "_"))
+  
+  # row 10
+  row_header_1 <- c(row_header_1, variable_names_full[[k]][6])
+  j <- descriptive_N_perc(j, paste0(variable_names_full[[k]][6], "_"))
+  
+  # row 11
+  if (length(variable_names_full[[k]])>=7){
+    row_header_1 <- c(row_header_1, variable_names_full[[k]][7])
+    j <- descriptive_N_perc(j, paste0(variable_names_full[[k]][7], "_"))
+  }
+
+  # row 12
+  if (length(variable_names_full[[k]])>=8){
+    row_header_1 <- c(row_header_1, variable_names_full[[k]][8])
+    j <- descriptive_N_perc(j, paste0(variable_names_full[[k]][8], "_"))
+  }  
+  # row 13
+  if (length(variable_names_full[[k]])>=9){
+    row_header_1 <- c(row_header_1, variable_names_full[[k]][9])
+    j <- descriptive_N_perc(j, paste0(variable_names_full[[k]][9], "_"))
+  }  
+  
+  # row 14
+  if (length(variable_names_full[[k]])>=10){
+    row_header_1 <- c(row_header_1, variable_names_full[[k]][10])
+    j <- descriptive_N_perc(j, paste0(variable_names_full[[k]][10], "_"))
+  }
+  
+  
+  
+  #########################################
+  # KEEP CELLS
+  
+  cell_cols <- grep("^cell_", names(tab_nice), value = TRUE)
+  tokeep <- c(cell_cols)
+  tab_nice <- tab_nice[, ..tokeep]
+  
+  
+  #########################################
+  # RESHAPE CELLS
+  
+  # First reshape tab_nice from wide into long format
+  
+  tab_nice <- melt(
+    tab_nice,
+    measure.vars = cell_cols,
+    variable.name = "rownum",
+    value.var = "value"
+  )
+  
+  tab_nice[, rownum := as.integer(sub("cell_", "", rownum))]
+  
+  setorder(tab_nice, rownum)
+  
+  
+  # then reshape from long to wide keeping rownum as the UoO
+  
+  #tab_nice <- dcast(
+    #tab_nice,
+    #rownum ~ character(0),
+    #value.var = "value"
+  #)
+  
+  
+  # Order rows correctly
+  #setorder(tab_nice, rownum)
+  
+  
+  #########################################
+  # ADD ROW HEADER
+  #
+  
+  tab_nice[, row_header := row_header_1]
+  tab_nice[, rownum := NULL]
+  
+  setcolorder(tab_nice, c("row_header", "value"))
+  #########################################
+  # FINAL COLUMNS
+  
+  #data_cols <- setdiff(names(tab_nice), "row_header")
+  #p1_cols   <- grep("^2016_", data_cols, value = TRUE)
+  #p2_cols   <- grep("^2020_", data_cols, value = TRUE)
+  #p3_cols <- setdiff(data_cols, c(p1_cols, p2_cols))
+  #setcolorder(tab_nice, c("row_header", p1_cols, p2_cols, p3_cols))
+  
+  #########################################
+  # NAMES
+  
+  # newnames <- c(
+  #   pre = "Pre- Nota 100 AIFA (1ge2016-25gen2022)",
+  #   nota = "Nota 100 AIFA (26gen2022-31lug2025)",
+  #   modifica = "Modifica Nota 100 AIFA(1ago2025-31dic2025)"
+  # )
+  # 
+  # 
+  # tab_nice[1] <- lapply(tab_nice[1], function(x) {
+  # 
+  #   idx <- x %in% names(newnames)
+  #   x[idx] <- unname(newnames[x[idx]])
+  #   x
+  # 
+  #  })
+  
+  
+  
+  #########################################
+  # SAVE
+  
+  outputfile <- tab_nice
+  nameoutput <- "D6_Table_2"
+  assign(nameoutput, outputfile)
+  
+  # rds
+  saveRDS(outputfile, file = file.path(thisdiroutput, paste0(nameoutput,"_", k,".rds")))
+  # csv
+  fwrite(outputfile, file = file.path(thisdiroutput, paste0(nameoutput,"_", k,".csv")))
+  # xls
+  write_xlsx(outputfile, file.path(thisdiroutput, paste0(nameoutput,"_", k,".xlsx")))
+  # html
+  # html_table <- kable(outputfile, format = "html", escape = FALSE) %>% kable_styling(full_width = F, bootstrap_options = c("striped", "hover"))
+  # writeLines(html_table, file.path(thisdiroutput, paste0(nameoutput,"_", k,".html")))
+  # rtf
+  doc <- read_docx() %>% body_add_table(outputfile, style = "table_template", header = F) %>% body_end_section_continuous()
+  print(doc, target = file.path(thisdiroutput, paste0(nameoutput,"_", k,".docx")))
+  
+}
