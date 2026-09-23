cbf42e68d565f4ee5510740dc6d516278aabf7a9
sabrinagiometto <sabrina.giometto@gmail.com>
2026-09-23 13:05:35 +0200

    D5 for Table 2 fixed changing how we identify the ATC IV level mostly used and results updated

--- p_steps/05_T4_30_Create_D5_Table_2_asl_2025.R
+++ p_steps/05_T4_30_Create_D5_Table_2_asl_2025.R
@@ -26,82 +26,85 @@ year <- year_p
 data <- readRDS(file = paste0(thisdirinput, "/D3_coorte_con_caratterizzazione_",year,".rds"))
 data <- as.data.table(data)
 
-atc <- unique(unlist(strsplit(data[,atc_5_almeno_3],"_")))
-atc_IV_levels <- unique(substring(atc, 1, 5))
-data[,(atc_IV_levels):=lapply(atc_IV_levels, function(x) as.integer(grepl(paste0("(^|_)", x), data[,atc_5_almeno_3])))]
+# atc <- unique(unlist(strsplit(data[,atc_5_almeno_3],"_")))
+# atc_IV_levels <- unique(substring(atc, 1, 5))
+# data[,(atc_IV_levels):=lapply(atc_IV_levels, function(x) as.integer(grepl(paste0("(^|_)", x), data[,atc_5_almeno_3])))]
 
-variable_names_full <- NULL
+# variable_names_full <- NULL
+
+atc_long <- data[,.(person_id, atc_5_almeno_3)]
+atc_long <- atc_long[, .(atc = unlist(strsplit(atc_5_almeno_3, " ", fixed = TRUE))), by = person_id]
+
+atc_IV_long <- copy(atc_long)
+atc_IV_long[, atc_IV:=substring(atc, 1, 5)]
+atc_IV_long <- unique(atc_IV_long, by = c("person_id", "atc_IV"))
+
+atc_IV_long[, atc:=NULL]
+
+toadd <- copy(data)[, asl := "Tutte"]
+data <- rbind(data, toadd)
 
 
 for (j in asl) {
 
+  D5 <- data[, .N, asl]
   
   # extract 10 most frequent ATC IV level
-  distr <- data.frame(N=1)
-
-  for (k in atc_IV_levels) {
-
-    tmp <- data.frame(sum(data[asl==j, get(k)==1]))
-
-    colnames(tmp) <- k
-
-    distr <- cbind(distr, tmp)
-
-  }
-
-  distr <- as.data.table(distr)
-
-  distr_l <- melt(distr[, N:=NULL],
-                  measure.vars = names(distr),
-                  variable.name = "variable",
-                  value.name = "atc")
-
-  distr_l <- setorder(distr_l, -atc)
-
-  distr_l_sel <- distr_l[c(1:10)]
-
-  variable_names <- distr_l_sel[, as.character(variable)]
+  # distr <- data.frame(N=1)
+  # 
+  # for (k in atc_IV_levels) {
+  # 
+  #   tmp <- data.frame(sum(data[asl==j, get(k)==1]))
+  # 
+  #   colnames(tmp) <- k
+  # 
+  #   distr <- cbind(distr, tmp)
+  # 
+  # }
+  # 
+  # distr <- as.data.table(distr)
+  # 
+  # distr_l <- melt(distr[, N:=NULL],
+  #                 measure.vars = names(distr),
+  #                 variable.name = "variable",
+  #                 value.name = "atc")
+  # 
+  # distr_l <- setorder(distr_l, -atc)
+  # 
+  # distr_l_sel <- distr_l[c(1:10)]
+  # 
+  # variable_names <- distr_l_sel[, as.character(variable)]
+  # 
+  # variable_names <- variable_names[!is.na(variable_names)]
   
-  variable_names <- variable_names[!is.na(variable_names)]
-
-
-  # create D5 with binary covariates
-  covariates_full <- variable_names
-
-  D5_cov <- NULL
-
-  for (i in covariates_full) {
-
-    tmp <- data[asl==j, .(
-                N = .N,
-                tmp_N = sum(get(i)==1),
-                tmp_p = round(sum(get(i)==1)/.N,3)*100)]
-
-    setnames(tmp,"tmp_N",paste0(i, "_N"))
-    setnames(tmp,"tmp_p",paste0(i, "_p"))
-
-    if (is.null(D5_cov)) {
-
-      D5_cov <- tmp
-
-    } else {
-
-      D5_cov <- merge(D5_cov, tmp, by = c("N"))
-    }
-
-  }
-
-  assign(paste0("D5_",j), D5_cov)
-  variable_names_full[[j]] <- variable_names
+  temp <- merge(atc_IV_long, data[,.(person_id, asl)], by = "person_id", all = F, allow.cartesian = T)
   
+  temp <- temp[, .N, by = c("atc_IV","asl")]
+  setorder(temp, asl, - N)
+  temp[, ord := seq(.N), by = asl]
+  temp <- temp[ord <= 10, ]
+  
+  wide <- dcast(temp, 
+                asl ~ ord, 
+                value.var = c("atc_IV", "N")
+  )
+  
+  setnames(wide, sub("^atc_IV_(\\d+)$", "combinazione_piu_utilizzata_\\1", names(wide)))
+  setnames(wide, sub("^N_(\\d+)$", "combinazione_piu_utilizzata_\\1_N", names(wide)))
+  
+  
+  D5 <- merge(D5, wide, by = "asl")
+  
+  for (k in 1:10) {
+    
+    D5[, paste0("combinazione_piu_utilizzata_",k, "_p"):=round(get(paste0("combinazione_piu_utilizzata_",k, "_N"))/N,3)*100]
+    
+  }
 
 }
 
-# save
-for (j in asl) {
 
-  saveRDS(get(paste0("D5_", j)), file = paste0(thisdiroutput, "/D5_Table_2_combinazioni_farmaci_",year,"_", j, ".rds"))
-  write.csv(get(paste0("D5_", j)), file = paste0(thisdirexp, "/D5_Table_2_combinazioni_farmaci_",year,"_", j, ".csv"))
+saveRDS(D5, file = paste0(thisdiroutput, "/D5_Table_2_combinazioni_farmaci_", year, ".rds"))
+write.csv(D5, file = paste0(thisdirexp, "/D5_Table_2_combinazioni_farmaci_", year, ".csv"))
 
-}
 
