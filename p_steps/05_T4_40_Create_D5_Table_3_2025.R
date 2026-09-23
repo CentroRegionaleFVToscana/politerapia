4a40847c744ab873e9dc973535e0276f1fb83ab6
Elena264-F <elenaferrati@gmail.com>
2026-09-23 11:51:44 +0200

    D5_ table 3

---
+++ p_steps/05_T4_40_Create_D5_Table_3_2025.R
@@ -0,0 +1,144 @@
+# author: Sabrina Giometto, Elena Ferrati
+
+# v 0.1 11 Sep 2026
+
+# prima draft
+
+#########################################
+
+if (TEST){
+  testname <- "test_D5_Tabella_3_2025"
+  thisdirinput <- file.path(dirtest,testname)
+  thisdiroutput <- file.path(dirtest,testname,"g_output")
+  thisdirexp <- thisdiroutput
+  dir.create(thisdiroutput, showWarnings = F)
+}else{
+  thisdirinput <- dirtemp
+  thisdiroutput <- dirtemp
+  thisdirexp <- direxp
+}
+
+year <- year_p
+
+# load data
+
+# dummy data
+# data <- read.csv(paste0(thisdirinput, "/D3_coorte_con_caratterizzazione_dummy.csv"), sep = ";")
+data <- readRDS(file = paste0(thisdirinput, "/D3_coorte_con_caratterizzazione_", year, ".rds"))
+data <- as.data.table(data)
+
+data_new <- data[rsa_adi == 1]
+atc <- unique(unlist(strsplit(data_new[,atc_5_almeno_3],"_")))
+# data[,(atc):=lapply(atc,function(x) as.integer(grepl(paste0("(^|_)", x, "(_|$)"),data[,atc_5_almeno_3])))]
+
+atc_long <- data_new[,.(person_id, atc_5_almeno_3)]
+atc_long <- atc_long[, .(atc = unlist(strsplit(atc_5_almeno_3, " ", fixed = TRUE))), by = person_id]
+
+
+
+# store names of the five drugs (V level) most frequently dispensed
+# variable_names_full <- NULL
+
+
+#for (j in fasce_eta) {
+  
+#  dataj <- copy(data)[fasciaeta == j,]
+  toadd <- copy(data_new)[, asl := "Tutte"]
+#  toadd <- toadd[, asl := "Tutte"]
+  data_new <- rbind(data_new, toadd)
+  
+  # Create D5 with sociodemographic characteristics
+  D5_nocov <- data_new[, .(
+    N          = .N,
+    genere_F_N = sum(genere=="F"),
+    genere_F_p = round(sum(genere=="F")/.N,3)*100),
+    
+  
+    
+    # aggiungere ulteriori covariate
+    .(asl)]
+  
+  for (i in fasce_eta) {
+      
+      tmp <- data_new[, .(N = .N, 
+                      paste0("fasciaeta_", i, "_N") = sum(fasciaeta==i)), .(asl)] 
+      D5_nocov <- merge(D5_nocov, tmp, by = "asl")
+    }
+  
+  # extract 5 most frequent ATC V level in this age band
+  
+  temp <- merge(atc_long, data_new[,.(person_id, asl)], by = "person_id", all = F, allow.cartesian = T)
+  
+  temp <- temp[, .N, by = c("atc","asl")]
+  setorder(temp, asl, - N)
+  temp[, ord := seq(.N), by = asl]
+  temp <- temp[ord <= 5, ]
+  
+  wide <- dcast(temp, 
+                asl ~ ord, 
+                value.var = c("atc", "N")
+  )
+  
+  setnames(wide, sub("^atc_(\\d+)$", "farmaco_piu_utilizzato_\\1", names(wide)))
+  setnames(wide, sub("^N_(\\d+)$", "farmaco_piu_utilizzato_\\1_N", names(wide)))
+  
+  
+  D5_nocov <- merge(D5_nocov, wide, by = "asl")
+  
+  for (k in 1:5) {
+    
+    D5_nocov[, paste0("farmaco_piu_utilizzato_",k, "_p"):=round(get(paste0("farmaco_piu_utilizzato_",k, "_N"))/N,3)*100]
+    
+  }
+  
+  
+  # variable_names <- distr_l_sel[, as.character(variable)]
+  
+  
+  # create D5 with binary covariates
+  covariates_binary <- c("iperpoliterapia", "cardiocircolatoria", "reumatologica", "gastroenterologica", "esenzione_qualsiasi")
+  
+  # covariates_full <- c(covariates_binary, variable_names)
+  
+  D5_cov <- NULL
+  
+  for (i in covariates_binary) {
+    
+    tmp <- data_new[, .(
+      N = .N,
+      tmp_N = sum(get(i)==1),
+      tmp_p = round(sum(get(i)==1)/.N,3)*100),
+      .(asl)]
+    
+    setnames(tmp,"tmp_N",paste0(i, "_N"))
+    setnames(tmp,"tmp_p",paste0(i, "_p"))
+    
+    if (is.null(D5_cov)) {
+      
+      D5_cov <- tmp
+      
+    } else {
+      
+      D5_cov <- merge(D5_cov, tmp, by = c("asl", "N"))
+    }
+    
+  }
+  
+
+  
+  # create the final D5 by merging the previous two
+  D5 <- merge(D5_nocov, D5_cov, by = c("asl","N"), all = F)
+  
+  #assign(paste0("D5_",j), D5)
+  
+  # variable_names_full[[j]] <- variable_names
+  
+}
+
+# save
+for (j in fasce_eta) {
+  
+  saveRDS(get(paste0("D5_", j)), file = paste0(thisdiroutput, "/D5_Tabella_1_caratterizzazione_",year, "_", j, ".rds"))
+  write.csv(get(paste0("D5_", j)), file = paste0(thisdirexp, "/D5_Tabella_1_caratterizzazione_",year,"_", j, ".csv"))
+  
+}
